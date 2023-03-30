0726c9d2c3d9bca2d5fcf1fa743a4cc2dfe296df
lciscon2 <lciscon@gmail.com>
2023-03-30 15:21:53 -0500

    Created initial directory structure and python library files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='pumpCtrl',
+    packages=find_packages(include=['pumpCtrl']),
+    version='0.1.0',
+    description='Pump control routines for liquid dispenser',
+    author='Larry Ciscon',
+    license='MIT',
+)
