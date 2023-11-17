4668748cdbb8ffd63a6643a93b9e02c08d5fbecf
lciscon <lciscon@yahoo.com>
2023-11-17 17:19:53 -0600

    various fixes

--- pumpCtrl/ccontrol.py
+++ pumpCtrl/ccontrol.py
@@ -1,3 +1,5 @@
+from adafruit_motorkit import MotorKit
+
 import time
 import atexit
 from pumpCtrl import config
@@ -7,7 +9,7 @@ mh = []
 # create a default object, no changes to I2C address or frequency
 for i in range(0, config.PUMPS_NUMHATS):
     haddr = 0x60 + i
-    newmh = MotorKit(addr=haddr, i2c_bus=config.I2C_BASEADDR) #busnum
+    newmh = MotorKit(address=haddr)
     mh.append(newmh)
 
 # recommended for auto-disabling motors on shutdown!
@@ -25,28 +27,47 @@ def calcRunTime(pnum, volume):
     rtime = volume/config.PUMP_CONFIG[pnum][config.PUMP_MLPS]
     return rtime
 
-def stopPump(pnum):
+def getMotor(pnum):
     ndx = (pnum% 4)+1
     if (ndx == 1):
-        myMotor = getHAT(pnum).motor1
+        mtr = getHAT(pnum).motor1
     elif (ndx == 2):
-        myMotor = getHAT(pnum).motor2
+        mtr = getHAT(pnum).motor2
     elif (ndx == 3):
-        myMotor = getHAT(pnum).motor3
-    elif (ndx == 4):
-        myMotor = getHAD(pnum).motor4
-   
-    myMotor.run(Adafruit_MotorHAT.RELEASE)
+        mtr = getHAT(pnum).motor3
+    else:
+        mtr = getHAT(pnum).motor4
+
+    return mtr
+
+def getDir(pnum):
+    ndx = (pnum% 4)+1
+    if (ndx == 1):
+        dir = -1.0
+    elif (ndx == 2):
+        dir = 1.0
+    elif (ndx == 3):
+        dir = -1.0
+    else:
+        dir = 1.0
+
+    return dir
+
+
+def stopPump(pnum):
+    myMotor = getMotor(pnum)
+    # myMotor.run(Adafruit_MotorHAT.RELEASE)
+    myMotor.throttle = 0.0
 
 def runPump(pnum, volume):
-    ndx = (pnum% 4)+1
-    myMotor = getHAT(pnum).getMotor(ndx)
+    myMotor = getMotor(pnum)
 
     rtime = calcRunTime(pnum, volume)
-    myMotor.setSpeed(config.PUMP_CONFIG[pnum][config.PUMP_FLOW_RATE])
-    myMotor.run(Adafruit_MotorHAT.FORWARD)
+    # myMotor.throttle(config.PUMP_CONFIG[pnum][config.PUMP_FLOW_RATE])
+    # myMotor.run(Adafruit_MotorHAT.FORWARD)
+    myMotor.throttle = getDir(pnum)
     time.sleep(rtime)
 
-    # turn on motor
-    myMotor.run(Adafruit_MotorHAT.RELEASE)
-  
+    # turn off motor
+    # myMotor.run(Adafruit_MotorHAT.RELEASE)
+    myMotor.throttle = 0.0
